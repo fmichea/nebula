348c462cc47ba6fda6ed22086c12a2f39aab4148
Franck Michea <franck.michea@gmail.com>
2013-10-31 14:57:50 +0100

    Move keyboard support to SDL 2.0.

--- src/keyboard/handlers/sdlkbhandler.hh
+++ src/keyboard/handlers/sdlkbhandler.hh
@@ -2,13 +2,18 @@
 # define SDLKBHANDLER_HH_
 # if _TARGET == 0
 
-#  include <SDL/SDL.h>
+#  include <SDL.h>
+#  include <pthread.h>
 
 #  include "keyboard/handlers/kbhandler.hh"
 
 class SDLKBHandler : public KBHandler {
 public:
     SDLKBHandler(KBEventHandler* eventhandler);
+    ~SDLKBHandler();
+
+private:
+    pthread_t thread_;
 };
 
 # endif // _TARGET == 0
