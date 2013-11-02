3598ced6c06d0e60d19dc077775c87de68fbdb17
Franck Michea <franck.michea@gmail.com>
2013-11-02 04:05:39 +0100

    Implement NRx4 register proxy class.

---
+++ src/memory/registers/nrx4.hh
@@ -0,0 +1,23 @@
+#ifndef NRX___HH_
+# define NRX___HH_
+
+# include "memory/registers/bitproxy.hh"
+# include "memory/registers/register.hh"
+
+class NRX4Proxy : public RegisterProxy {
+    NRX4Proxy() : RegisterProxy() {}
+
+    NRX4Proxy(uint8_t* reg, uint16_t addr)
+        : RegisterProxy(reg, addr)
+    {
+        this->restart = BitProxy(reg, 7, 0x1);
+        this->repeat = BitProxy(reg, 6, 0x1);
+        this->freq_hi = BitProxy(reg, 0, 0x7);
+    }
+
+    BitProxy restart;
+    BitProxy repeat;
+    BitProxy freq_hi;
+};
+
+#endif // !NRX___HH_
