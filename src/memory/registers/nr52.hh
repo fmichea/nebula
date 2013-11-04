8b75d0c01681a4f8f96a8fb1dae4b75539cb3aaf
Franck Michea <franck.michea@gmail.com>
2013-11-04 18:31:25 +0100

    Implement special register NR52.

---
+++ src/memory/registers/nr52.hh
@@ -0,0 +1,25 @@
+#ifndef NR52_HH_
+# define NR52_HH_
+
+# include "bitproxy.hh"
+# include "register.hh"
+
+# include "../../sound/constants.hh"
+
+class NR52Proxy : public RegisterProxy {
+public:
+    NR52Proxy() : RegisterProxy() {}
+
+    NR52Proxy(uint8_t* reg, uint16_t addr)
+        : RegisterProxy(reg, addr)
+    {
+        this->sound_on = BitProxy(reg, 7, 0x1);
+        for (unsigned int it = 0; it < NB_GB_CHANNELS; ++it)
+            this->channel_on[it] = BitProxy(reg, it, 0x1);
+    }
+
+    BitProxy sound_on;
+    BitProxy channel_on[NB_GB_CHANNELS];
+};
+
+#endif // !NR52_HH_
