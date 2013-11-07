7e56931e2888d8358ce73cc84c402b9ef7c3aced
Franck Michea <franck.michea@gmail.com>
2013-11-07 11:28:00 +0100

    Add a filter that produces a sin wave from a steady frequency.
      -> Last commit saying so was actually the clannels frequencies
         translator.

--- src/sound/filters/wave.hh
+++ src/sound/filters/wave.hh
@@ -6,13 +6,16 @@
 
 # include "../../logging.hh"
 # include "../../memory/registers/nrx1.hh"
+# include "../../memory/registers/nrx4.hh"
+# include "../../memory/registers/register.hh"
+# include "../../utils/converter.hh"
 # include "../../utils/cycle.hh"
 # include "../constants.hh"
 # include "filter.hh"
 
 class WaveForm : public Filter {
 public:
-    WaveForm(const NRX1Proxy& nrx1);
+    WaveForm(const NRX1Proxy& nrx1, const RegisterProxy& nrx3, const NRX4Proxy& nrx4);
 
     void reload();
     int32_t filter(int32_t frequency);
@@ -20,7 +23,13 @@ public:
 private:
     Cycle<unsigned int> frame_;
     const NRX1Proxy& nrx1_;
-    unsigned int duty_;
+    const RegisterProxy& nrx3_;
+    const NRX4Proxy& nrx4_;
+
+    Converter duty_tick_;
+    Cycle<unsigned int> duty_phase_;
+    bool duty_reload_;
+    unsigned int duty_pattern_;
 };
 
 #endif // !WAVE_HH_
